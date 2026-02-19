b7c7e8f8d7cabef45690304eea6525fd5724e7cf
Dharshini Vasu <dharshinivasu917@gmail.com>
2026-02-19 21:26:15 +0530

    Add files via upload

---
+++ java basics/jbasic21.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class jbasic21 {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int maintenance = sc.nextInt();
+        int parking=sc.nextInt();
+        int hall=sc.nextInt();
+        int security=sc.nextInt();
+        int electricity=sc.nextInt();
+        int cleaning=sc.nextInt();
+        int finalbill;
+        finalbill=maintenance+parking+hall-security-electricity-cleaning;
+        System.out.println(finalbill);
+        sc.close();
+
+
+    }
+    
+}
