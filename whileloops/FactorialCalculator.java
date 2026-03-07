c0adb78a0ff5d750cf8843cb180a6a6cdeb7f6fb
Dharshini Vasu <dharshinivasu917@gmail.com>
2026-03-07 21:08:58 +0530

    Add files via upload

---
+++ whileloops/FactorialCalculator.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class FactorialCalculator {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int fact=1;
+        int i=1;
+        while(i<=n){
+            fact=fact*i;
+
+          i++;
+        }
+        System.out.println("Factorial of "+n+" = "+fact);
+        sc.close();
+    }
+    
+}
