b35a360c535b6a5abca0fb4312ae378a201fffb6
Dharshini Vasu <dharshinivasu917@gmail.com>
2026-03-08 22:32:47 +0530

    Add files via upload

---
+++ whileloops/decimaltobinaryconvertor.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class decimaltobinaryconvertor {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int decimal=sc.nextInt();
+        int n=decimal;
+        int binary=0;
+        int place=1;
+        while (n>0) {
+            int rem=n%2;
+            binary=binary+rem*place;
+            place=place*10;
+            n=n/2;
+        }
+        System.out.println("Decimal: "+decimal);
+        System.out.println("Binary: "+binary);
+        sc.close();
+    }
+}
