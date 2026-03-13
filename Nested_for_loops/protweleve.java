c19b2013997ad44333afb7e104be11970983baba
Dharshini Vasu <dharshinivasu917@gmail.com>
2026-03-13 14:08:48 +0530

    Add files via upload

---
+++ Nested_for_loops/protweleve.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class protweleve {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        for(int i=0;i<size;i++){
+            for(int j=0;j<size;j++){
+                if((i+j)%2==0){
+                    System.out.print("■ ");
+                }
+                else {
+                    System.out.print("\u25A1 " );
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+        
+    }
+    
+}
