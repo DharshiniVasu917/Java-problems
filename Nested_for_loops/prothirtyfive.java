de8bcfedfcc16a79038eabc7e6abd9e7250038f1
Dharshini Vasu <dharshinivasu917@gmail.com>
2026-03-26 20:56:00 +0530

    Create prothirtyfive

---
+++ Nested_for_loops/prothirtyfive.java
@@ -0,0 +1,8 @@
+Test case 1
+Enter·element·of·first·node·:·15	
+Enter·element·of·second·node·:·18	
+The·list·is·:·15·-->·18·-->·NULL⏎	
+Test case 2
+Enter·element·of·first·node·:·10	
+Enter·element·of·second·node·:·20	
+The·list·is·:·10·-->·20·-->·NULL⏎
