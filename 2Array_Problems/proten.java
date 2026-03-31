c8d17ae1d796b8f704fa9f91f0fbd50e7240bbcb
Dharshini Vasu <dharshinivasu917@gmail.com>
2026-03-31 16:55:30 +0530

    Add files via upload

---
+++ 2Array_Problems/proten.java
@@ -0,0 +1,30 @@
+class البروten
+{
+    public static void main(String[] args)
+    {
+        int[][] a = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int rows = a.length;
+        int cols = a[0].length;
+
+        for(int i = 0; i < rows; i++)
+        {
+            for(int j = 0; j < cols; j++)
+            {
+                if(i == 0 || i == rows - 1 || j == 0 || j == cols - 1)
+                {
+                    System.out.print(a[i][j] + " ");
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
