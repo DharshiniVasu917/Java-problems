87334393f9731ff8ea637dd32f1c4547df425ea3
Dharshini Vasu <dharshinivasu917@gmail.com>
2026-04-04 16:43:37 +0530

    Add files via upload

--- 2Array_Problems/proten.java
+++ 2Array_Problems/proten.java
@@ -1,30 +1,45 @@
-class البروten
-{
-    public static void main(String[] args)
-    {
-        int[][] a = {
-            {1, 2, 3},
-            {4, 5, 6},
-            {7, 8, 9}
-        };
+import java.util.*;
 
-        int rows = a.length;
-        int cols = a[0].length;
+public class proten {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        for(int i = 0; i < rows; i++)
-        {
-            for(int j = 0; j < cols; j++)
-            {
-                if(i == 0 || i == rows - 1 || j == 0 || j == cols - 1)
-                {
-                    System.out.print(a[i][j] + " ");
-                }
-                else
-                {
-                    System.out.print("  ");
-                }
-            }
-            System.out.println();
+        int n = sc.nextInt();
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
         }
+
+        int min = a[0];
+        int max = a[0];
+        int sum = 0;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (a[i] < min) {
+                min = a[i];
+            }
+
+            if (a[i] > max) {
+                max = a[i];
+            }
+
+            sum = sum + a[i];
+
+            if (a[i] > 200) {
+                count++;
+            }
+        }
+
+        double avg = (double) sum / n;
+        double score = ((double)(n - count) / n) * 100;
+
+        System.out.println("Total Servers: " + n);
+        System.out.println("Fastest Response: " + min + "ms");
+        System.out.println("Slowest Response: " + max + "ms");
+        System.out.printf("Average Response: %.2fms\n", avg);
+        System.out.println("Slow Servers (>200ms): " + count);
+        System.out.printf("Performance Score: %.2f%%\n", score);
     }
 }
