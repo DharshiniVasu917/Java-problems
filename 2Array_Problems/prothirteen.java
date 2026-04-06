0450f82c7831462bebb62e33368c58c085c95186
Dharshini Vasu <dharshinivasu917@gmail.com>
2026-04-06 16:36:09 +0530

    Add files via upload

--- 2Array_Problems/prothirteen.java
+++ 2Array_Problems/prothirteen.java
@@ -1,25 +1,45 @@
-class prothirteen
-{
-    public static void main(String[] args)
-    {
-        int[][] a = {
-            {7, 2, 9},
-            {4, 1, 6}
-        };
+import java.util.*;
 
-        int min = a[0][0];
+public class prothirteen {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        for(int i = 0; i < a.length; i++)
-        {
-            for(int j = 0; j < a[i].length; j++)
-            {
-                if(a[i][j] < min)
-                {
-                    min = a[i][j];
-                }
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        double sum = 0;
+        int max = arr[0];
+        int onTime = 0;
+        int comp = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+
+            if (arr[i] <= 15) {
+                onTime++;
+            }
+
+            if (arr[i] > 180) {
+                comp++;
             }
         }
 
-        System.out.println(min);
+        double avg = sum / n;
+        double percent = (onTime * 100.0) / n;
+
+        System.out.println("Total Flights: " + n);
+        System.out.printf("Average Delay: %.2f minutes\n", avg);
+        System.out.println("Maximum Delay: " + max + " minutes");
+        System.out.println("On-Time Flights: " + onTime);
+        System.out.println("Compensation Required: " + comp);
+        System.out.printf("On-Time Performance: %.2f%%\n", percent);
     }
 }
