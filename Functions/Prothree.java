c199cd38df6aa4b1776b4fb659ccc816d14078b8
Dharshini Vasu <dharshinivasu917@gmail.com>
2026-04-19 17:12:33 +0530

    Add files via upload

---
+++ Functions/Prothree.java
@@ -0,0 +1,17 @@
+public class Main {
+    static int sumDigits(int n) {
+        int sum = 0;
+
+        while(n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int n = 1234;
+        System.out.println(sumDigits(n));
+    }
+}
