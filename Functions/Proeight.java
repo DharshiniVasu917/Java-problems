b3dfd522ef8c7a786c3bc30b14ae9ddf5da434b1
Dharshini Vasu <dharshinivasu917@gmail.com>
2026-04-21 17:24:07 +0530

    Add files via upload

---
+++ Functions/Proeight.java
@@ -0,0 +1,19 @@
+public class Main {
+    static int gcd(int a, int b) {
+        while(b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    static int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
+    }
+
+    public static void main(String[] args) {
+        int a = 12, b = 18;
+        System.out.println(lcm(a, b));
+    }
+  }
