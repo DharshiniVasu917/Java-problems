33f1fff50de85a658089d0aec4abb48adcd5d17f
Dharshini Vasu <dharshinivasu917@gmail.com>
2026-04-23 20:04:18 +0530

    Add files via upload

---
+++ Functions/Profifteen.java
@@ -0,0 +1,18 @@
+public class Main {
+    static int largest(int[] arr) {
+        int max = arr[0];
+
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 25, 7, 40, 15};
+        System.out.println(largest(arr));
+    }
+}
