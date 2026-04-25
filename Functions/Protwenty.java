7000904eb316d9b5251f304677b88ef9f77ac4ce
Dharshini Vasu <dharshinivasu917@gmail.com>
2026-04-25 22:17:28 +0530

    Add files via upload

---
+++ Functions/Protwenty.java
@@ -0,0 +1,10 @@
+public class Main {
+    static int square(int n) {
+        return n * n;
+    }
+
+    public static void main(String[] args) {
+        int n = 6;
+        System.out.println(square(n));
+    }
+}
