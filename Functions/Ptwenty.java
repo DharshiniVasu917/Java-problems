41bb3e1a8045c3536cedd095f94b09f04d4567bc
Dharshini Vasu <dharshinivasu917@gmail.com>
2026-04-27 22:16:20 +0530

    Add files via upload

---
+++ Functions/Ptwenty.java
@@ -0,0 +1,10 @@
+public class Main {
+    static int cube(int n) {
+        return n * n * n;
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        System.out.println(cube(n));
+    }
+}
