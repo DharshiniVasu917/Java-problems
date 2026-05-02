f5c18be9cc89ac9d1f88d8aeb3da9d06e903ca8d
Dharshini Vasu <dharshinivasu917@gmail.com>
2026-05-02 18:13:51 +0530

    Add files via upload

---
+++ Functions/Prthree.java
@@ -0,0 +1,21 @@
+public class Main {
+    static boolean hasDuplicate(String[] files) {
+        for(int i = 0; i < files.length; i++) {
+            for(int j = i + 1; j < files.length; j++) {
+                if(files[i].equals(files[j])) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String[] files = {"Main.java", "App.java", "Main.java"};
+
+        if(hasDuplicate(files))
+            System.out.println("Duplicate Found");
+        else
+            System.out.println("No Duplicates");
+    }
+}
