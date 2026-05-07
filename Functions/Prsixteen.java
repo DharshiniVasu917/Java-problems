80847afde6ee394988955cad17010d793a9ec35f
Dharshini Vasu <dharshinivasu917@gmail.com>
2026-05-07 20:50:20 +0530

    Add files via upload

---
+++ Functions/Prsixteen.java
@@ -0,0 +1,18 @@
+public class Main {
+    static String smallestFile(String[] files) {
+        String min = files[0];
+
+        for(int i = 1; i < files.length; i++) {
+            if(files[i].length() < min.length()) {
+                min = files[i];
+            }
+        }
+
+        return min;
+    }
+
+    public static void main(String[] args) {
+        String[] files = {"Main.java", "A.java", "Application.java"};
+        System.out.println(smallestFile(files));
+    }
+                            }
