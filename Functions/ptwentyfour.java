6f606b72678b5ee38df9a8d52f88e1e7ac6ee3d9
Dharshini Vasu <dharshinivasu917@gmail.com>
2026-05-13 21:44:25 +0530

    Add files via upload

---
+++ Functions/ptwentyfour.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+
+        int sum = add(10, 20);
+
+        System.out.println("Sum = " + sum);
+
+    }
+}
