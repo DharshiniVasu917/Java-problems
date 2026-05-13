ea6ae6dc5af5c4eea5c161e31cf0d56940aa8446
Dharshini Vasu <dharshinivasu917@gmail.com>
2026-05-13 21:45:09 +0530

    Add files via upload

---
+++ Functions/ptwentyfive.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    static void check(int n) {
+
+        if (n % 2 == 0) {
+            System.out.println("Even Number");
+        } else {
+            System.out.println("Odd Number");
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        check(7);
+
+    }
+}
