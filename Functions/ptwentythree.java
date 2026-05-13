c5b2e9d05308420501e7da291f33128c79469c12
Dharshini Vasu <dharshinivasu917@gmail.com>
2026-05-13 21:43:10 +0530

    Add files via upload

---
+++ Functions/ptwentythree.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    static void display() {
+        System.out.println("Hello Java");
+    }
+
+    public static void main(String[] args) {
+
+        display();
+
+    }
+}
