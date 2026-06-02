3953817ac394416bd90bd4d5c2c83be8afc5a4a4
Dharshini Vasu <dharshinivasu917@gmail.com>
2026-06-02 21:26:10 +0530

    Add files via upload

---
+++ Functions/P1.java
@@ -0,0 +1,35 @@
+public class Main {
+
+    public static double findMedian(int[] a, int[] b) {
+        int[] merged = new int[a.length + b.length];
+
+        int i = 0, j = 0, k = 0;
+
+        while(i < a.length && j < b.length) {
+            if(a[i] < b[j])
+                merged[k++] = a[i++];
+            else
+                merged[k++] = b[j++];
+        }
+
+        while(i < a.length)
+            merged[k++] = a[i++];
+
+        while(j < b.length)
+            merged[k++] = b[j++];
+
+        int n = merged.length;
+
+        if(n % 2 == 0)
+            return (merged[n/2 - 1] + merged[n/2]) / 2.0;
+        else
+            return merged[n/2];
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, 3};
+        int[] b = {2, 4};
+
+        System.out.println(findMedian(a, b));
+    }
+}
