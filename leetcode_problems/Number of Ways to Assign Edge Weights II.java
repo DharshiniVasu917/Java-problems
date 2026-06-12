8d1c3d298b97a10908e56ca21d2b0964aa357cb8
Dharshini Vasu <dharshinivasu917@gmail.com>
2026-06-12 20:59:38 +0530

    Add files via upload

---
+++ leetcode_problems/Number of Ways to Assign Edge Weights II.java
@@ -0,0 +1,69 @@
+class Solution {
+    private static final int MOD = 1_000_000_007;
+    private static final int LOG = 18;
+
+    public int[] assignEdgeWeights(int[][] edges, int[][] queries) {
+        int n = edges.length + 1;
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i <= n; i++) adj.add(new ArrayList<>());
+
+        for (int[] e : edges) {
+            adj.get(e[0]).add(e[1]);
+            adj.get(e[1]).add(e[0]);
+        }
+
+        int[] depth = new int[n + 1];
+        int[][] up = new int[n + 1][LOG];
+
+        boolean[] visited = new boolean[n + 1];
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        visited[1] = true;
+        up[1][0] = 1;
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            for (int nei : adj.get(node)) {
+                if (!visited[nei]) {
+                    visited[nei] = true;
+                    depth[nei] = depth[node] + 1;
+                    up[nei][0] = node;
+                    queue.add(nei);
+                }
+            }
+        }
+
+        for (int j = 1; j < LOG; j++)
+            for (int i = 1; i <= n; i++)
+                up[i][j] = up[up[i][j - 1]][j - 1];
+
+        long[] pow2 = new long[n + 1];
+        pow2[0] = 1;
+        for (int i = 1; i <= n; i++)
+            pow2[i] = pow2[i - 1] * 2 % MOD;
+
+        int[] ans = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            int u = queries[i][0], v = queries[i][1];
+            int lca = lca(u, v, depth, up);
+            int k = depth[u] + depth[v] - 2 * depth[lca];
+            ans[i] = (k == 0) ? 0 : (int) pow2[k - 1];
+        }
+        return ans;
+    }
+
+    private int lca(int u, int v, int[] depth, int[][] up) {
+        if (depth[u] < depth[v]) { int t = u; u = v; v = t; }
+        int diff = depth[u] - depth[v];
+        for (int j = 0; j < LOG; j++)
+            if (((diff >> j) & 1) == 1)
+                u = up[u][j];
+        if (u == v) return u;
+        for (int j = LOG - 1; j >= 0; j--)
+            if (up[u][j] != up[v][j]) {
+                u = up[u][j];
+                v = up[v][j];
+            }
+        return up[u][0];
+    }
+}
